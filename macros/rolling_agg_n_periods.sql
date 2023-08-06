07d57dda8fabb75a3157eb2a666a82c061fe4376
Heidi E. Schmidt <bb00p04@gmail.com>
2023-08-06 21:33:27 +0000

    modified macro to use defaults and general aggregation

---
+++ macros/rolling_agg_n_periods.sql
@@ -0,0 +1,9 @@
+{% macro rolling_agg_n_periods(column_name, partition_by, order_by, agg_type="avg", periods="7") %}
+
+    {{ agg_type }}({{ column_name }}) OVER (
+        PARTITION BY {{ partition_by }}
+        ORDER BY {{ order_by }}
+        ROWS BETWEEN {{ num_preceding_rows - 1 }} PRECEDING AND CURRENT ROW
+    ) AS {{ agg_type }}{{ periods }}_periods_{{ column_name }}
+
+{% endmacro %}
